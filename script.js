6931287700d654bfd6d2dbf9dd8c5465c1d4ef48
mathias782 <145474322+mathias782@users.noreply.github.com>
2024-05-25 17:21:09 +0200

    Controle + grid
    Controle op het formulier en het begin van de css grid

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 'use strict'
 
-document.getElementById('reis-form').addEventListener("submit", antwoordSubmit);
+document.getElementById('submit-knop').addEventListener("click", controleSubmit);
 
 function antwoordSubmit(event){
     event.preventDefault();
     let antwoord = document.getElementById('antwoord');
-    const html = `<p>Submitten is gelukt!</p>`;
-    antwoord.innerHTML = html;
+    antwoord.innerHTML = `<p>Submitten is gelukt!</p>`;
     antwoord.style.color = "green";
+}
+
+function controleSubmit(event){
+    let land = document.getElementById('land').value;
+    let stad = document.getElementById('stad').value;
+    // let beschrijving = document.getElementById('beschrijving-reis').value;
+    let cijfer = document.getElementById('reis-cijfer').value;
+    event.preventDefault();
+    if(land == ""){
+        let antwoord = document.getElementById('antwoord');
+        antwoord.innerHTML = `<p>Je moet het land van jouw reis nog invullen</p>`;
+        antwoord.style.color = "red";
+        antwoord.style.textAlign = "center";
+    }else if(stad == ""){
+        let antwoord = document.getElementById('antwoord');
+        antwoord.innerHTML = `<p>Je moet de stad van jouw reis nog invullen</p>`;
+        antwoord.style.color = "red";
+    }else if(cijfer == ""){
+        let antwoord = document.getElementById('antwoord');
+        antwoord.innerHTML = `<p>Je moet het cijfer van jouw reis nog invullen</p>`;
+        antwoord.style.color = "red";
+    }else if(cijfer < 1 || cijfer > 10){
+        let antwoord = document.getElementById('antwoord');
+        antwoord.innerHTML = `<p>Het cijfer van jouw reis moet tussen 1 en 10 zijn</p>`;
+        antwoord.style.color = "red";
+    }else{
+        antwoordSubmit(event);
+    }
 }
